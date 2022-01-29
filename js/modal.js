96bb39c935118496b0e28b7421b9b3e6e6d9c9bf
Garvit Verma <garv8tverma26@gmail.com>
2022-01-29 22:24:03 +0530

    Create modal.js

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("myModal");
+var trigg = document.getElementById("modal-greet");
+
+var span = document.getElementsByClassName("close")[0];
+
+trigg.onload = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
